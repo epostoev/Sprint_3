29509365d96d2fcb0ebbcd847992185f0bda9bcf
Your Name <you@example.com>
2026-03-30 16:45:42 +0300

    26.03.30 16:45 - added new method add_item_to_cheque

--- main.py
+++ main.py
@@ -15,8 +15,21 @@ class OnlineSalesRegisterCollector:
     def number_items(self):
         return self.__number_items
 
+    def add_item_to_cheque(self, name):
+        if len(name) == 0 or len(name) > 40:
+            raise ValueError("'Нельзя добавить товар, если в его названии нет символов или их больше 40'")
+        if name not in self.__item_price:
+            raise NameError("Позиция отсутствует в товарном справочнике")
+        else:
+            self.__name_items.append(name)
+            self.__number_items += 2
 
-kassa_1 = OnlineSalesRegisterCollector()
 
-print(kassa_1.name_items)
-print(kassa_1.number_items)
+# kassa_1 = OnlineSalesRegisterCollector()
+
+# print(kassa_1.name_items)
+# print(kassa_1.number_items)
+# kassa_1.add_item_to_cheque("чипсы")
+# print(kassa_1.name_items)
+# print(kassa_1.number_items)
+
